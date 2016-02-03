d8443e7b3d6d602d768f4ce8dc264104c2b8ee4d
Hari Samala <hari.samala@eyefinity.com>
2016-02-03 12:06:35 -0800

    Initial commit

---
+++ src/main/java/com/github/cicd/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.github.cicd.example;
+
+/**
+ * Description: TODO
+ * @author Hari Samala
+ * @since Feb 3, 2016
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out
+				.println("Hello world from rhels-gradle-info-plugin-consumer.");
+	}
+}
